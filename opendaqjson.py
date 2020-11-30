884e3c4a3768fa24419b5de19874e75450bfbac8
ArantzazuSt <arantzazusola@gmail.com>
2020-11-30 18:21:15 +0100

    se mejora la estructura

--- opendaqjson.py
+++ opendaqjson.py
@@ -1,44 +1,40 @@
-#Fijar un valor en la salida y leer las entradas seleccionadas
+'''Programa que imprime el valor de las entrads al fijar un valor en la salida '''
 
 import json
 import argparse
-import time 
+import time
 
 from opendaq import *
 
 
+if __name__ == 'main':
 
-#funcion argparse
+    parser = argparse.ArgumentParser(description='Valor de las salidas Opendaq')
 
-parser = argparse.ArgumentParser(description='Valor de las salidas Opendaq')
+    parser.add_argument('-p', '--puerto', metavar='', help='Puerto seleccionado')
+    parser.add_argument('-l', '--listInput', nargs='+', metavar='',
+                        type=int, help='Entradas seleccionadas')
+    parser.add_argument('-r', '--rep', metavar='', type=int,
+                        help='Numero de repeticiones')
 
-parser.add_argument('-p','--puerto', metavar= '', help='Puerto seleccionado')
-parser.add_argument('-l','--listInput',nargs='+', metavar='', type= int, help='Entradas seleccionadas' )
-parser.add_argument('-r','--rep', metavar='', type= int, help='Numero de repeticiones')
+    args = parser.parse_args()
+    daq = DAQ(args.puerto)
 
-args=parser.parse_args()
+    mylist = args.listInput  # Array de entradas a leer
 
-#FALTA: def NumeroRepeticiones
+    f = open("prueba.json", 'w')
+    data = {
+        "model": daq.hw_ver,
+        "serial": daq.serial_str,
+        "port": args.puerto,
+        # "time": int(time.time()),
+        "items": []
 
-daq = DAQ(args.puerto)
+    }
 
-mylist = args.listInput      #Array de entradas a leer
+    for i in mylist:
+        data["items"].append({"Input number": i, "readings": daq.read_analog()})
+        print(data)
 
-f=open("prueba.json",'w')
-data={       
-        "model":daq.hw_ver, 
-        "serial":daq.serial_str,
-        "port":args.puerto,
-        #"time": int(time.time()),
-        "items":[]      
-        
-     }   
-
-for i in mylist:
-    data["items"].append({"Input number":i, "readings":daq.read_analog()})
-    print (data)
-
-import json
-json.dump(data, f, indent= 2)
-f.close()
-
+    json.dump(data, f, indent=2)
+    f.close()
